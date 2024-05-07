e78c8bc5dae7a63558ae204d61e16267ef13ce3e
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-05-07 18:09:23 -0300

    Add files via upload

---
+++ python/exercicio8.py
@@ -0,0 +1,12 @@
+# co= float(input("Digite o comprimento do cateto oposto:"))
+# ca = float(input("Digite o comprimento do catetto adjacente:"))
+# hi= (co**2 + ca**2)**(1/2)
+# print("A hipotenusa vai medir {:.2f}".format(hi))
+
+# from math import hypot
+# co = float(input("Digite o comprimento do cateto oposto"))
+# ca = float(input("Digite o comprimento do cateto adjacente"))
+# hi = hypot(co, ca)
+# print("A hiponusa vai medir {:.2f}".format(hi))
+
+an = float(input(""))
