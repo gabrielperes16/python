38d20ab6a336e2280d87fa0943aa24e07f84f5be
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-05-10 01:59:30 -0300

    Add files via upload

--- python/manipulação_caracteres.py
+++ python/manipulação_caracteres.py
@@ -1,2 +1,9 @@
-frase = ("Curso em video")
-print(("Curso" in frase),frase[:16])
+nome = input("Insira seu nome completo:")
+nome_Maiusculo = (nome.upper())
+lista= nome.split()
+Letras = len(nome)
+leitor = len(lista[0])
+
+print("O nome em maiusculo é {}".format(nome_Maiusculo))
+print("A quantidade total de Letras com espaços é {}".format(Letras))
+print("A quantidade de caracteres no primeiro nome é",leitor)
