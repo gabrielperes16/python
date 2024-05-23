0899b2e53a6a0b19c0ee4f105ddc8e3f151bc911
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-05-23 18:51:31 -0300

    Add files via upload

--- python/cores_terminal.py
+++ python/cores_terminal.py
@@ -1,1 +1,6 @@
-print("\033[30mHello word!\033[m")
+nome = str(input("Digite seu nome: "))
+cores = {
+    "limpa": "\033[m",
+    "azul": "\033[36m"
+}
+print("prazer em te conhecer {}{}{}!!!".format(cores['azul'], nome, cores['limpa']))
