be27228c3296b20f6313e54e8171220937b7711b
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-05-27 19:00:50 -0300

    Add files via upload

--- python/pythonMundo2/exercicio39.py
+++ python/pythonMundo2/exercicio39.py
@@ -1,8 +1,30 @@
+import sys
+print('-=-'*20)
+print('''
+        Caso seu sexo seja MASCULINO tecle: [1]
+        Caso seu sexo seja FEMININO tecle:  [2]\n''')
+print('-=-'*20)
+
+sexo= int(input("Qual o seu sexo? "))
+if sexo == 1:
+    print("O sexo escolhido é o MASCULINO!!!")
+elif sexo == 2:
+    print("O sexo escolhido é o FEMININO!!!")
+else:
+    print("Valor inválido!!!")
+
+if sexo == 1:
+    print("O sexo masculino pode se alistar!!!")
+elif sexo == 2:
+    print("O sexo feminino não pode se alistar!!!")
+    sys.exit(0)
+else:
+    print("Valor inválido!!!")
 ano_nascimento = int(input("Digite seu ano de nascimento: "))
-idade= 2024-ano_nascimento
+idade = 2024-ano_nascimento
 anos_restantes = 18-idade
-ano_alistamento= ano_nascimento+18
-print("Quem nasceu me {} tem {} anos em {}".format(ano_nascimento,idade,2024))
+ano_alistamento = ano_nascimento+18
+print("Quem nasceu me {} tem {} anos em {}".format(ano_nascimento, idade, 2024))
 if idade >= 18:
     print("Você já deveria ter se alistado há {} anos".format(idade-18))
 else:
