7ad1cadb29f084e47128104c0f9ed47c2a39b132
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-06-11 18:16:21 -0300

    Add files via upload

--- python/pythonMundo2/exercicio61.py
+++ python/pythonMundo2/exercicio61.py
@@ -1,13 +1,12 @@
-repetiçoes=0
-print("=="*20)
+print("\033[32m=="*20)
 print("          10 TERMOS DE UMA PA")
-print("=="*20)
-n=0
+print("=="*20,'\033[m')
 primeiro_termo = int(input("Digite o valor do primeiro termo: "))
 razão = int(input("Digite o valor da RAZÂO: "))
-while repetiçoes==10:
-    termos =primeiro_termo+(10-1) *razão
-    n=primeiro_termo+ (termos+1)*razão
-repetiçoes+=1
-print(f"A PA do valor {primeiro_termo} e o valor da razão {razão} é igual a {n}", end=" ↦ ")
-print("ACABOU!")
+termo= primeiro_termo
+contador=1
+while contador <= 10:
+    print(f"\033[32m{termo} ↦\033[m", end=" ")
+    termo+= razão
+    contador+=1
+print("\033[01;36mFIM\033[m")
