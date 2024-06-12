fd47827d86b05b4014e0a508afc58704f24230ea
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-06-12 01:33:49 -0300

    Add files via upload

--- python/pythonMundo2/exercicio61.py
+++ python/pythonMundo2/exercicio61.py
@@ -1,18 +1,19 @@
 print("\033[32m=="*20)
 print("          10 TERMOS DE UMA PA")
-print("=="*20,'\033[m')
+print("=="*20, '\033[m')
 primeiro_termo = int(input("Digite o valor do primeiro termo: "))
 razão = int(input("Digite o valor da RAZÂO: "))
-termo= primeiro_termo
+termo = primeiro_termo
 contador=1
-while contador <= 10:
-    print(f"\033[32m{termo} ↦\033[m", end=" ")
-    termo+= razão
-    contador+=1
-print("PAUSA")
-add_termos=0
-contador==10
-while contador+add_termos:
-    add_termos= int(input("Quantos termos a mais você precisa? "))
-    contador+=add_termos
-print(f"{contador}↦",end="")
+total=0
+add_termos=10
+while add_termos !=0:
+    total=total+add_termos
+    while contador <= total:
+        print(f"\033[32m{termo} ↦\033[m", end=" ")
+        termo += razão
+        contador += 1
+    print("PAUSA")
+    add_termos = int(input("\033[01;36mQuantos termos a mais você precisa?\033[m"))
+print(f"\033[01;36mA quantidade de termos usados foi \033[01;32m{total}\033[m")
+print("\033[32mFIM\033[m")
