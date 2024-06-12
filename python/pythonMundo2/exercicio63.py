26788824a88031e601012e7bd41edfa201124e16
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-06-12 11:36:35 -0300

    Add files via upload

--- python/pythonMundo2/exercicio63.py
+++ python/pythonMundo2/exercicio63.py
@@ -2,12 +2,14 @@ print("\033[01;32m=="*11)
 print("Sequencia de Fibonacci")
 print("=="*11)
 n = int(input("\033[36mDigite quantos termos você quer mostrar: "))
-t1 = 0
-t2 = 1
-cont=3
-print("\033[32m~\033[m"*30)
-while cont<=n:
-    cont+=1
+t1=0
+t2=1
+print(f"{t1} , {t2} ,", end=" ")
+cont= 3
+while cont<= n:
     t3= t1+t2
-    t4= t3+t2
-print(f"\033[01;36m{t1} ↦  {t2}  ↦ {t3}  ↦ {t4}")
+    t1=t2
+    t2=t3
+    cont+=1
+    print(f"{t3} , ", end=" ")
+print("FIM")
