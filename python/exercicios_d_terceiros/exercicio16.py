3b107b6082a2f714ea86f78a4bd039edd639ff6a
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-06-14 00:05:18 -0300

    Add files via upload

--- python/exercicios_d_terceiros/exercicio16.py
+++ python/exercicios_d_terceiros/exercicio16.py
@@ -1,4 +1,13 @@
-from time import date
-pais_a= 80000
-pais_b= 200000
-print("tempo:")
+cont = 0
+pais_a = 80000
+pais_b = 200000
+calculo_a = (pais_a/100)*103
+calculo_b= (pais_b/100)*101.5
+while pais_a < pais_b:
+    calculo_a = (pais_a/100)*103
+    calculo_b= (pais_b/100)*101.5
+    pais_a=calculo_a
+    pais_b=calculo_b
+    cont += 1
+
+print(f'Foi nescessario {cont} anos para o pais A ultrapaçar o pais B com {calculo_a:.2f}')
