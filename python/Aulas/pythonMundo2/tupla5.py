c32d4dba7acbd8a3cda78c1970d79904fc2f0c8e
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-06-18 23:55:11 -0300

    Add files via upload

--- python/Aulas/pythonMundo2/tupla5.py
+++ python/Aulas/pythonMundo2/tupla5.py
@@ -1,9 +1,10 @@
+compra=''
 while True:
     produtos=str(input("digite o nome dos produtos:"))
     preco=float(input("Digite o preco dos produtos:"))
     continuar=str(input("Deseja continuar[ s ] ou [ n ]".lower().strip()))
     if continuar=='n':
         break
-compra=produtos,preco
-print(compra)
+    compra+=produtos,preco
+print(f'{compra}')
 
